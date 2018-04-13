628a703e83d8a2702a9138ea086a6f98c5945f03
EC2 Default User <ec2-user@ip-172-31-37-223.us-east-2.compute.internal>
2018-04-13 16:36:28 +0000

    another commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,21 +6,23 @@ class HomeController < ApplicationController
 
   def show
     @my_lotto=(1..45).to_a.sample(6).sort
-    
+    # my_lotto : 무작위 수 6개 추출
     
     require('open-uri') 
     require('json')
     page=JSON.parse(open('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=801').read).sort
     
     @num = Array.new
+       # num : 당첨 번호 배열
     
     page.each do |x,v|
       if x.include?('drwtNo')
        @num << v
       end
     end
-    
+ 
     @num1 = 0
+    # num1 : 보너스 당첨 번호
     
     page.each do |x,v|
       if x.include?('bnusNo')
@@ -30,6 +32,9 @@ class HomeController < ApplicationController
    
      @a = Array.new
      @b=0
+     
+    # a : 내 번호와 당첨 번호의 맞는 숫자를 넣은 배열
+    # b : 보너스 번호가 나왔을 때
    
     @num.each do |x|
       if @my_lotto.include?(x)
@@ -45,6 +50,7 @@ class HomeController < ApplicationController
   end
   
   @rank = 999999
+  # rank : 내 등수
  
   if @a.include?(@num1)
     if @a.count == 6
